3c7c6d5bcf9642b3c82dc682e342196048ba7e34
Yoshikazu <>
2019-03-18 08:39:34 +0900

    プロジェクトを追加

---
+++ SampleQiitaListProject/SampleQiitaListProject/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  SampleQiitaListProject
+//
+//  Created by Yoshikazu on 2019/03/18.
+//  Copyright © 2019 YoshikazuIshii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+       let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func updateViewConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
